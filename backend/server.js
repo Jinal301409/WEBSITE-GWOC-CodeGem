eed39d13761294985295bf577debe7c416a85335
Jinal301409 <jinalpatel898047@gmail.com>
2026-01-25 15:05:45 +0530

    admin frontend add

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ connectDB()
 //MIDDLEWARE
 app.use(cors({
     origin: (origin, callback) => {
-        const allowedOrigins = ['https://website-gwoc-codegem.onrender.com', 'https://website-gwoc-codegem-admin.onrender.com/']
+        const allowedOrigins = ['https://website-gwoc-codegem.onrender.com', 'https://website-gwoc-codegem-admin.onrender.com']
         if(!origin || allowedOrigins.includes(origin)) {
             callback(null, true)
         }
